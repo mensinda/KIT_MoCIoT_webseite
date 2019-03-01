eae46c91f580ab241dffe30f6c977c030580df96
Daniel Mensinger <daniel@mensinger-ka.de>
2019-03-01 22:34:49 +0100

    Some more playing around

---
+++ src/components/SensorData.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='sensor'>
+    <h1>Dies ist ein Test</h1>
+    <p>Test: {{test_str}}</p>
+    <p>Comp: {{comp_str}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SensorData extends Vue {
+  test_str: string = 'asdff'
+
+  get comp_str(): string {
+    return this.$store.state.const_str
+  }
+}
+</script>
--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    const_str: 'Global store string'
   },
   mutations: {
 
